783c48b6f8fee1f10ba7b643e846ef475d7d4440
Joaov511 <jv232479@gmail.com>
2025-03-17 11:19:34 -0300

    update file names

---
+++ changeResolution.bat
@@ -0,0 +1,3 @@
+@echo off
+set "currentDir=%~dp0"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%currentDir%resolutionToggleScript.ps1'"
--- chres.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set "currentDir=%~dp0"
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%currentDir%changeDisplayResolution.ps1'"
